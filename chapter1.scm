a1b49d7f61b92fdaa353175dba3cc1cb92966b7b
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-13 19:10:21 +0800

    add count-change function

--- chapter1.scm
+++ chapter1.scm
@@ -276,3 +276,22 @@
       b
       (iter (+ a b) a (- count 1))))
   (iter 1 0 n))
+
+;; count change
+(define (count-change amount)
+  (define (first-denomination kinds-of-coins)
+    (cond ((= kinds-of-coins 1) 1)
+	  ((= kinds-of-coins 2) 5)
+	  ((= kinds-of-coins 3) 10)
+	  ((= kinds-of-coins 4) 25)
+	  ((= kinds-of-coins 5) 50)))
+  (define (cc amount kinds-of-coins)
+    (cond ((= amount 0) 1)
+	  ((or (< amount 0) (= kinds-of-coins 0)) 0)
+	  (else (+ (cc amount
+		       (- kinds-of-coins 1))
+		   (cc (- amount
+			  (first-denomination
+			    kinds-of-coins))
+		       kinds-of-coins)))))
+  (cc amount 5))
