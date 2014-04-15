200e2e1dbce7c90d191d2d39aa3ec0895521c65c
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-15 19:35:49 +0800

    exercise 1.17 done

--- chapter1.scm
+++ chapter1.scm
@@ -396,3 +396,18 @@
 		      base
 		      (- counter 1)))))
   (iter 1 b n))
+
+;; Exercise 1.17
+(define (double n)
+  (+ n n))
+(define (halve n)
+  (/ n 2))
+(define (mul a b)
+  (if (= b 0)
+    0
+    (+ a (mul a (- b 1)))))
+(define (fast-mul-recursive a b)
+  (cond ((= b 0) 0)
+	((even? b) (fast-mul (double a) (halve b)))
+	(else
+	  (+ a (fast-mul a (- b 1))))))
