781813d3af4cf4824428d458b29a9a476f413113
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-15 23:33:35 +0800

    exercise 1.20 done

--- chapter1.scm
+++ chapter1.scm
@@ -452,3 +452,50 @@
   (if (= b 0)
     a
     (gcd b (remainder a b))))
+
+;; Exercise 1.20
+;; normal order 需要计算 1+2+4+7+4 = 18次
+;(gcd 206 40)
+;(if (= 40 0)
+  ;206
+  ;(gcd 40 (remainder 206 40)))
+
+;(gcd 40 (remainder 206 40))
+;(if (= (remainder 206 40) 0) ;; 6
+  ;40
+  ;(gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+;(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+;(if (= (remainder 40 (remainder 206 40))) ;; 4
+  ;(remainder 206 40)
+  ;(gcd (remainder 40 (remainder 206 40))
+       ;(remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+
+;(gcd (remainder 40 (remainder 206 40))
+     ;(remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+;(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0) ;; 2
+  ;(remainder 40 (remainder 206 40))
+  ;(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+       ;(remainder (remainder 40 (remainder 206 40))
+		  ;(remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
+
+;; applicative order需要计算 4次。
+;(gcd 206 40)
+;(if (= 40 0)
+  ;206
+  ;(gcd 40 (remainder 206 40)))
+
+;(gcd 40 6)
+;(if (= 6 0)
+  ;40
+  ;(gcd 6 (remainder 40 6)))
+
+;(gcd 6 4)
+;(if (= 4 0)
+  ;6
+  ;(gcd 4 (remainder 6 4)))
+
+;(gcd 4 2)
+;(if (= 2 0)
+  ;4
+  ;(gcd 2 (remainder 4 2)))
