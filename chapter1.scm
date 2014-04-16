fc8a467faed24399c9226d663b0682f2bb9be972
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-16 16:39:29 +0800

    add fermat test for prime

--- chapter1.scm
+++ chapter1.scm
@@ -511,3 +511,22 @@
   (find-divisor n 2))
 (define (prime? n)
   (= n (smallest-divisor n)))
+
+;; 使用费马小定理来做质数检测
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (expmod (square base) (halve exp) m)
+		    m))
+	(else
+	  (remainder (* base
+			(expmod base (- exp 1) m))
+				     m))))
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else #f)))
