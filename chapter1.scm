3160d4a62dd45295912703e177dcee88c66320d6
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-18 15:43:31 +0800

    exercise 1.28 done

--- chapter1.scm
+++ chapter1.scm
@@ -752,3 +752,42 @@
 ;; => #t
 ;; (test-carmichael-number 6601)
 ;; => #t
+
+;; Exercise 1.28
+(define (expmod-with-check base exp m)
+  (define (square-with-check x m)
+    (if (and (not (or (= x 1) (= x (- m 1))))
+	     (= (remainder (square x) m) 1))
+      0
+      (remainder (square x) m)))
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (square-with-check
+	   (expmod-with-check base (/ exp 2) m) m))
+	(else
+	  (remainder (* base
+			(expmod-with-check base (- exp 1) m))
+				     m))))
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod-with-check a (- n 1) n) 1))
+  (try-it (+ 1 (random (- n 1)))))
+
+;; (miller-rabin-test 561)
+;; => #f
+;; (miller-rabin-test 562)
+;; => #f
+;; (miller-rabin-test 563)
+;; => #t
+;; (miller-rabin-test 1105)
+;; => #f
+;; (miller-rabin-test 1729)
+;; => #f
+;; (miller-rabin-test 2465)
+;; => #f
+;; (miller-rabin-test 2821)
+;; => #f
+;; (miller-rabin-test 6601)
+;; => #f
+;; (miller-rabin-test 6601)
+;; => #f
