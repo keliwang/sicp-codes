b21804c3799e1fa10574163c48fc062066765601
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-18 16:05:58 +0800

    完成三个sum类的函数以及一个通用的sum函数模板

--- chapter1.scm
+++ chapter1.scm
@@ -791,3 +791,38 @@
 ;; => #f
 ;; (miller-rabin-test 6601)
 ;; => #f
+
+;; 将Procedure作为参数
+;; (define (sum-integers a b)
+;;   (if (> a b)
+;;     0
+;;     (+ a (sum-integers (+ a 1) b))))
+;; (define (sum-cubes a b)
+;;   (if (> a b)
+;;     0
+;;     (+ (cube a) (sum-cubes (+ a 1) b))))
+;; (define (pi-sum a b)
+;;   (if (> a b)
+;;     0
+;;     (+ (/ 1.0 (* a (+ a 2)))
+;;        (pi-sum (+ a 4) b))))
+;; 上面的三个函数有着明显相同的模式，下面我们将这种模式抽象出来
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2)))) 
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
