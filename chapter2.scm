10f632f46886868ae685756a97effdaabaf9078a
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-23 16:13:32 +0800

    exercise 2.25 done

--- chapter2.scm
+++ chapter2.scm
@@ -498,3 +498,11 @@
 ;; (list 1 (list 2 (list 3 4)))
 ;; 解释器的输出：(1 (2 (3 4)))
 ;; box-and-pointer图及树形图省略。
+
+;; Exercise 2.25
+;; (1 3 (5 7) 9)
+;; => (car (cdr (car (cdr (cdr lst)))))
+;; ((7))
+;; => (car (car lst))
+;; (1 (2 (3 (4 (5 (6 7))))))
+;; => (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr lst))))))))))))
