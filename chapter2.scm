b15be9ae8dfc2956bde4b8464b17ec34e793bc60
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-23 16:16:00 +0800

    exercise 2.26 done

--- chapter2.scm
+++ chapter2.scm
@@ -506,3 +506,13 @@
 ;; => (car (car lst))
 ;; (1 (2 (3 (4 (5 (6 7))))))
 ;; => (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr lst))))))))))))
+
+;; Exercise 2.26
+;; (define x (list 1 2 3))
+;; (define y (list 4 5 6))
+;; (append x y)
+;; => (1 2 3 4 5 6)
+;; (cons x y)
+;; => ((1 2 3) 4 5 6)
+;; (list x y)
+;; => ((1 2 3) (4 5 6))
