30d7ce4f769ef7599fcb9bfea13f2477628f7982
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-24 15:47:13 +0800

    exercise 2.30 done

--- chapter2.scm
+++ chapter2.scm
@@ -611,3 +611,17 @@
 	   (scale-tree sub-tree factor)
 	   (* sub-tree factor)))
        tree))
+
+;; Exercise 2.30
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+	((not (pair? tree)) (square tree))
+	(else
+	  (cons (square-tree (car tree))
+		(square-tree (cdr tree))))))
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	   (square-tree sub-tree)
+	   (square sub-tree)))
+       tree))
