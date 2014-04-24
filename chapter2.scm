da98dc7094f9a78472315ba63a1b8d326ecedf0a
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-24 15:49:42 +0800

    exercise 2.31 done

--- chapter2.scm
+++ chapter2.scm
@@ -625,3 +625,13 @@
 	   (square-tree sub-tree)
 	   (square sub-tree)))
        tree))
+
+;; Exercise 2.31
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	   (tree-map proc sub-tree)
+	   (proc sub-tree)))
+       tree))
+(define (square-tree tree)
+  (tree-map square tree))
