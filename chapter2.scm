e8d691f7958903e512fc33d922159d992c25f614
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-25 11:01:54 +0800

    exercise 2.37 done

--- chapter2.scm
+++ chapter2.scm
@@ -737,3 +737,15 @@
     nil
     (cons (accumulate op init (map car seqs))
 	  (accumulate-n op init (map cdr seqs)))))
+
+;; Exercise 2.37
+;; 矩阵相关操作
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product v x)) m))
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
