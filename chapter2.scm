d56ae069a920d5679677d219d197fffd908ba933
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-25 09:32:55 +0800

    将序列作为接口的一些示例

--- chapter2.scm
+++ chapter2.scm
@@ -649,3 +649,65 @@
 ;; and the power set of any other set is all the subsets of the set
 ;; containing some specific element and all the subsets of the set not
 ;; containing that specific element.
+
+;; 将序列作为接口
+(define (fib n)
+  (define (iter a b count)
+    (if (= count 0)
+      b
+      (iter (+ a b) a (- count 1))))
+  (iter 1 0 n))
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+	((not (pair? tree))
+	 (if (odd? tree) (square tree) 0))
+	(else (+  (sum-odd-squares (car tree))
+		  (sum-odd-squares (cdr tree))))))
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+      nil
+      (let ((f (fib k)))
+	(if (even? f)
+	  (cons f (next (+ k 1)))
+	  (next (+ k 1))))))
+  (next 0))
+
+(define (my-filter predicate sequence)
+  (cond ((null? sequence) nil)
+	((predicate (car sequence))
+	 (cons (car sequence)
+	       (my-filter predicate (cdr sequence))))
+	(else (my-filter predicate (cdr sequence)))))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+	(accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+	((not (pair? tree)) (list tree))
+	(else (append (enumerate-tree (car tree))
+		      (enumerate-tree (cdr tree))))))
+(define (sum-odd-squares tree)
+  (accumulate
+    + 0 (map square (my-filter odd? (enumerate-tree tree)))))
+(define (even-fibs n)
+  (accumulate
+    cons
+    nil
+    (my-filter even? (map fib (enumerate-interval 0 n)))))
+(define (list-fib-squares n)
+  (accumulate
+    cons
+    nil
+    (map square (map fib (enumerate-interval 0 n)))))
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+	      1
+	      (map square (my-filter odd? sequence))))
