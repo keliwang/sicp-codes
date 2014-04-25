af417c47b172e203732fa49f4feee76615dc360e
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-25 10:11:22 +0800

    exercise 2.34 done

--- chapter2.scm
+++ chapter2.scm
@@ -719,3 +719,9 @@
   (accumulate cons seq2 seq1))
 (define (my-length sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+;; Exercise 2.34
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ (* higher-terms x) this-coeff))
+	      0
+	      coefficient-sequence))
