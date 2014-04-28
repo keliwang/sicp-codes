1f46f006c108987f34505d950758b6e8bb424e99
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-28 16:40:18 +0800

    exercise 2.50 done

--- chapter2.scm
+++ chapter2.scm
@@ -1084,3 +1084,14 @@
 	  (lambda (frame)
 	    (paint-left frame)
 	    (paint-right frame))))))
+
+;; Exercise 2.50
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 0.0 1.0)))
+(define (rotate180 painter)
+  (rotate90 (rotate90 painter)))
+(define (rotate270 painter)
+  (rotate90 (rotate180 painter)))
