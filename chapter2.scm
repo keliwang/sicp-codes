1b4d80cc0005c1c73c064896bb73dd14e9bf2ffa
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-28 16:55:20 +0800

    exercise 2.51 done

--- chapter2.scm
+++ chapter2.scm
@@ -1095,3 +1095,22 @@
   (rotate90 (rotate90 painter)))
 (define (rotate270 painter)
   (rotate90 (rotate180 painter)))
+
+;; Exercise 2.51
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-top
+	    (transform-painter painter2)
+	    split-point
+	    (make-vect 1.0 0.5)
+	    (make-vect 0.0 1.0))
+	  (paint-bottom
+	    (transform-painter painter1)
+	    (make-vect 0.0 0.0)
+	    (make-vect 1.0 0)
+	    split-point))
+      (lambda (frame)
+	(paint-top frame)
+	(paint-bottom frame)))))
+(define (below painter1 painter2)
+  (rotate90 (beside painter1 painter2)))
