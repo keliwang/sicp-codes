37a0201dd38812dc8c6a2c59f00848fa1542c248
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-29 09:42:22 +0800

    exercise 2.53 done

--- chapter2.scm
+++ chapter2.scm
@@ -1133,3 +1133,17 @@
   (cond ((null? x) #f)
 	((eq? item (car x)) x)
 	(else (my-memq item (cdr x)))))
+
+;; Exercise 2.53
+;; (list 'a 'b 'c)
+;; => (a b c)
+;; (list (list 'george))
+;; => ((george))
+;; (cdr '((x1 x2) (y1 y2)))
+;; => ((y1 y2))
+;; (pair? (car '(a short list)))
+;; => #f
+;; (memq 'red '((red shoes) (blue socks)))
+;; => #f
+;; (memq 'red '(red shoes blue socks))
+;; => (red shoes blue socks)
