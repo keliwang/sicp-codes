158e2bd927b5612917338a9668968cc47a8baaf6
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-29 15:37:25 +0800

    exercise 2.56 done
    Signed-off-by: Edgar Wang <edgar.xiaoxiangyu@gmail.com>

--- chapter2.scm
+++ chapter2.scm
@@ -1219,3 +1219,33 @@
         (else (list '* m1 m2))))
 
 
+;; Exercise 2.56
+(define (exponentiation? exp)
+  (and (pair? exp) (eq? (car exp) '**)))
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+	((=number? e 1) b)
+	(else (list '** b e))))
+(define (base s)
+  (cadr s))
+(define (exponent s)
+  (caddr s))
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+	((variable? exp) (if (same-variable? exp var) 1 0))
+	((sum? exp)
+	 (make-sum (deriv (addend exp) var)
+		   (deriv (augend exp) var)))
+	((product? exp)
+	 (make-sum
+	  (make-product (multiplier exp)
+			(deriv (multiplicand exp) var))
+	  (make-product (deriv (multiplier exp) var)
+			(multiplicand exp))))
+	((exponentiation? exp)
+	 (make-product (exponent exp)
+		       (make-product
+			(make-exponentiation (base exp) (- (exponent exp) 1))
+			(deriv (base exp) var))))
+	(else
+	 (error "deriv" "unknown expression type" exp))))
