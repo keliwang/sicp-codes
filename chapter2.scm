6a1daa80d96554fa8a420ae01077510a66ae7420
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-30 08:02:14 +0800

    exercise 2.59 done

--- chapter2.scm
+++ chapter2.scm
@@ -1314,3 +1314,11 @@
 	 (cons (car set1) (intersection-set (cdr set1) set2)))
 	(else
 	 (intersection-set (cdr set1) set2))))
+
+;; Exercise 2.59
+(define (union-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) set2)
+	((element-of-set? (car set1) set2)
+	 (union-set (cdr set1) set2))
+	(else
+	 (cons (car set1) (union-set (cdr set1) set2)))))
