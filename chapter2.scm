45a6e303597ef959741d3730df0a84f57c323d96
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-30 08:16:09 +0800

    exercise 2.60 done

--- chapter2.scm
+++ chapter2.scm
@@ -1322,3 +1322,20 @@
 	 (union-set (cdr set1) set2))
 	(else
 	 (cons (car set1) (union-set (cdr set1) set2)))))
+
+;; Exercise 2.60
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+	((equal? x (car set) #t))
+	(else
+	 (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (cons x set))
+(define (union-set set1 set2)
+  (append set1 set2))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1) (intersection-set (cdr set1) set2)))
+	(else
+	 (intersection-set (cdr set1) set2))))
