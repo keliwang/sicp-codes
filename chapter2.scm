d99659c43b31ead444ead08c729513a94d003387
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-30 16:50:04 +0800

    exercise 2.66 done

--- chapter2.scm
+++ chapter2.scm
@@ -1533,3 +1533,13 @@
 	 (car set-of-records))
 	(else
 	 (lookup given-key (cdr set-of-records)))))
+
+;; Exercise 2.66
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) #f)
+	((= given-key (key (entry set-of-records)))
+	 (car set-of-records))
+	((< given-key (key (entry set-of-records)))
+	 (lookup given-key (right-branch set-of-records)))
+	(else
+	 (lookup given-key (left-branch set-of-records)))))
