9b4c388565c29683914d9c91cf003a57d08ff15d
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-04-30 09:49:50 +0800

    sets as ordered lists

--- chapter2.scm
+++ chapter2.scm
@@ -1344,3 +1344,22 @@
 ;; 由于set里允许有重复的元素，所以它的空间消耗会变大。
 ;; 这种表示方法主要应该用在我们不关心空间消耗，同时
 ;; 我们执行union-set和adjoin-set操作比较频繁时采用。
+
+;; sets as ordered lists
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+	((= x (car set)) #t)
+	((< x (car set)) #f)
+	(else
+	 (element-of-set? x (cdr set)))))
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+	(cond ((= x1 x2)
+	       (cons x1 (intersection-set (cdr set1)
+					  (cdr set2))))
+	      ((< x1 x2)
+	       (intersection-set (cdr set1) set2))
+	      ((< x2 x1)
+	       (intersection-set set1 (cdr set2)))))))
