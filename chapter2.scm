06a715b22910f33fc58d161f3205373889a07401
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-01 16:14:36 +0800

    add adjoin-set and make-leaf-set for Huffman

--- chapter2.scm
+++ chapter2.scm
@@ -1588,3 +1588,18 @@
 	((= bit 1) (right-branch branch))
 	(else
 	 (error "choose-branch" "bad bit" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set)))
+	 (cons x set))
+	(else
+	 (cons (car set)
+	       (adjoin-set x (cdr set))))))
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(adjoin-set (make-leaf (car pair) ; symbol
+			       (cadr pair)) ; frequency
+		    (make-leaf-set (cdr pairs))))))
