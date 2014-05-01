cf47a7d844097b68ae570be2b866952bb55b968e
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-01 16:24:31 +0800

    exercise 2.67 done

--- chapter2.scm
+++ chapter2.scm
@@ -1603,3 +1603,15 @@
 	(adjoin-set (make-leaf (car pair) ; symbol
 			       (cadr pair)) ; frequency
 		    (make-leaf-set (cdr pairs))))))
+
+;; Exercise 2.67
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+		  (make-code-tree
+		   (make-leaf 'B 2)
+		   (make-code-tree
+		    (make-leaf 'D 1)
+		    (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+;; (decode sample-message sample-tree)
+;; => (A D A B B C A)
