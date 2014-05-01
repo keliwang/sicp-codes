ef3ae18906ce10a6efc507e60878fe8df3804176
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-01 15:36:30 +0800

    add Huffman decoding procedure

--- chapter2.scm
+++ chapter2.scm
@@ -1570,3 +1570,21 @@
   (if (leaf? tree)
       (weight-leaf tree)
       (cadddr tree)))
+
+;; Huffman decoding procedure
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+	'()
+	(let ((next-branch
+	       (choose-branch (car bits) current-branch)))
+	  (if (leaf? next-branch)
+	      (cons (symbol-leaf next-branch)
+		    (decode-1 (cdr bits) tree))
+	      (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+	((= bit 1) (right-branch branch))
+	(else
+	 (error "choose-branch" "bad bit" bit))))
