612b32e3fd45e74f7226b342c5ddd034b64d46db
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-01 15:28:24 +0800

    add Huffman encoding procedure

--- chapter2.scm
+++ chapter2.scm
@@ -1543,3 +1543,30 @@
 	 (lookup given-key (right-branch set-of-records)))
 	(else
 	 (lookup given-key (left-branch set-of-records)))))
+
+
+;; Huffman encoding procedure
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+	right
+	(append (symbols left) (symbols right))
+	(+ (weight left) (weight right))))
+(define (left-branch tree)
+  (car tree))
+(define (right-branch tree)
+  (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
