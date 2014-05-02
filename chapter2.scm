7745760ea3988646849d65915690076aa4338bd7
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-02 20:56:51 +0800

    add complex number's representations

--- chapter2.scm
+++ chapter2.scm
@@ -1681,3 +1681,44 @@
 ;; 编码最少出现的符号则需要沿着树一直向下，一共要向下n次，
 ;; 一共需要进行n次查找，其查找次数为n+(n-1)+...+1=n^2，
 ;; 所以其复杂度为O(n^2)。
+
+;; complex number
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+		       (+ (imag-part z1) (imag-part z2))))
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+		       (- (imag-part z1) (imag-part z2))))
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		     (+ (angle z1) (angle z2))))
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		     (- (angle z1) (angle z2))))
+
+;; 第一种实现方法
+(define (real-part z) (car z))
+(define (imag-part z) (cdr z))
+(define (magnitude z)
+  (sqrt (+ (square (real-part z))
+	   (square (imag-part z)))))
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+(define (make-from-real-imag x y)
+  (cons x y))
+(define (make-from-mag-ang r a)
+  (cons (* r (cos a)) (* r (sin a))))
+;; 第二种实现方法
+(define (real-part z)
+  (* (magnitude z) (cos (angle z))))
+(define (imag-part z)
+  (* (magnitude z) (sin (angle z))))
+(define (magnitude z)
+  (car z))
+(define (angle z)
+  (cdr z))
+(define (make-from-real-imag x y)
+  (cons (sqrt (+ (square x) (square y)))
+	(atan y x)))
+(define (make-from-mag-ang r a)
+  (cons r a))
