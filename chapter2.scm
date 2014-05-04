be1c5b61e5e5efa1d20c0412db4876a3fa250025
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-04 10:42:44 +0800

    exercise 2.75 done

--- chapter2.scm
+++ chapter2.scm
@@ -1975,3 +1975,13 @@
 	   (error "make-from-real-imag" "Unknown op" op))))
   dispatch)
 (define (apply-generic op arg) (arg op))
+
+;; Exercise 2.75
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'magnitude) r)
+	  ((eq? op 'angle) a)
+	  ((eq? op 'real-part) (* r (cos a)))
+	  ((eq? op 'imag-part) (* r (sin a)))
+	  (else
+	   (error "make-from-mag-ang" "Unknown op" op)))))
