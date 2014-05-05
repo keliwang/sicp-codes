ff4477dba6cdaf1a216204f46b32c26bd0d13e83
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-05 11:56:41 +0800

    exercise 2.80 done

--- chapter2.scm
+++ chapter2.scm
@@ -2148,3 +2148,16 @@
 	 (and (= (real-part x) (real-part y))
 	      (= (imag-part x) (imag-part y)))))
   'done)
+
+;; Exercise 2.80
+(define (=zero? x)
+  (apply-generic '=zero? x))
+(define (install-=zero?-package)
+  (put '=zero? '(scheme-number)
+       (lambda (x) (= x 0)))
+  (put '=zero? '(rational)
+       (lambda (x) (= (numer x) 0)))
+  (put '=zero? '(complex)
+       (lambda (x) (and (= (real-part x) 0)
+			(= (imag-part x) 0))))
+  'done)
