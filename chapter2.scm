b98cc1de701dd5f9a8fdd013b54fb6ae4311b7c6
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-05 11:52:39 +0800

    exercise 2.79 done

--- chapter2.scm
+++ chapter2.scm
@@ -2132,3 +2132,19 @@
 	 (cdr datum))
 	(else
 	 (error "contents" "datum is not valid" datum))))
+
+;; Exercise 2.79
+(define (equ? x y)
+  (apply-generic 'equ? x y))
+(define (install-equ?-package)
+  (put 'equ? '(scheme-number scheme-number)
+       (lambda (x y) (= x y)))
+  (put 'equ? '(rational rational)
+       (lambda (x y)
+	 (= (* (numer x) (denom y))
+	    (* (numer y) (denom x)))))
+  (put 'equ? '(complex complex)
+       (lambda (x y)
+	 (and (= (real-part x) (real-part y))
+	      (= (imag-part x) (imag-part y)))))
+  'done)
