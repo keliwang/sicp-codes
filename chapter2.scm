fc397bbb4c0fff265e614aa192a4fe80d5977e30
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-06 10:35:55 +0800

    add coercion example

--- chapter2.scm
+++ chapter2.scm
@@ -2161,3 +2161,29 @@
        (lambda (x) (and (= (real-part x) 0)
 			(= (imag-part x) 0))))
   'done)
+
+;; Coercion，即类型转换
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+(put-coercion 'scheme-number
+	      'complex
+	      scheme-number->complex)
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+	  (apply proc (map contents args))
+	  (if (= (length args) 2)
+	      (let ((type1 (car type-tags))
+		    (type2 (cadr type-tags))
+		    (a1 (car args))
+		    (a2 (cadr args)))
+		(let ((t1->t2 (get-coercion type1 type2))
+		      ((t2->t1 (get-coercion type2 type1))))
+		  (cond (t1->t2
+			 (apply-generic op (t1->t2 a1) a2))
+			(t2->t1
+			 (apply-generic op a1 (t2->t1 a2)))
+			(else
+			 (error "apply-generic" "No method for these types" (list op type-tags))))))
+	      (error "apply-generic" "No method for these types" (list op type-tags)))))))
