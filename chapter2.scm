26f3e38e5564b045643679121fe5ac00769fcf83
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-10 10:57:10 +0800

    exercise 2.93 done

--- chapter2.scm
+++ chapter2.scm
@@ -2808,3 +2808,42 @@
   'done)
 (define (make-dense-polynomial var terms)
   ((get 'make 'dense-polynomial) var terms))
+
+;; Exercise 2.92: TODO
+
+;; Exercise 2.93
+(define (install-rational-package)
+  ;; internal procedure
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (cons n d))
+  (define (add-rat x y)
+    (make-rat (add (mul (numer x) (denom y))
+		 (mul (numer y) (denom x)))
+	      (mul (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (sub (mul (numer x) (numer y))
+		 (mul (numer y) (numer x)))
+	      (mul (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (mul (numer x) (numer y))
+	      (mul (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (mul (numer x) (denom y))
+	      (mul (denom x) (numer y))))
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
