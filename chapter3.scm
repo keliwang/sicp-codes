5c2b6331fdaf7b02db3ee85c74f2ded8088352c9
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-19 14:40:25 +0800

    add a new cons implementation with assignment operation

--- chapter3.scm
+++ chapter3.scm
@@ -439,3 +439,33 @@
 	  (else
 	   (detect-cycle (get-slow slow) (get-fast fast)))))
   (detect-cycle (get-slow lst) (get-fast lst)))
+
+
+(define (my-cons x y)
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+	  ((eq? m 'cdr) y)
+	  (else (error "MY-CONS" "Undefined operation" m))))
+  dispatch)
+(define (my-car z) (z 'car))
+(define (my-cdr z) (z 'cdr))
+
+(define (my-cons x y)
+  (define (set-x! v) (set! x v))
+  (define (set-y! v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+	  ((eq? m 'cdr) y)
+	  ((eq? m 'set-car!) set-x!)
+	  ((eq? m 'set-cdr!) set-y!)
+	  (else
+	   (error "MY-CONS" "Undefined operation" m))))
+  dispatch)
+(define (my-car z) (z 'car))
+(define (my-cdr z) (z 'cdr))
+(define (my-set-car! z new-value)
+  ((z 'set-car!) new-value)
+  z)
+(define (my-set-cdr! z new-value)
+  ((z 'set-cdr!) new-value)
+  z)
