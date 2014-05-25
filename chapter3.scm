f4ac54c8cd3d5b1b4d41c8438e5d8e721c73d45d
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-25 10:22:36 +0800

    add and-gate and inverter

--- chapter3.scm
+++ chapter3.scm
@@ -889,3 +889,29 @@
     (half-adder a s sum c2)
     (or-gate c1 c2 c-out)
     'OK))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+		   (lambda () (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'OK)
+(define (logical-not s)
+  (cond ((= s 0) 1)
+	((= s 1) 0)
+	(else (error "LOGICAL-NOT" "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+	   (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+		   (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'OK)
+(define (logical-and a b)
+  (cond ((and (= a 1) (= b 1)) 1)
+	((or (= a 0) (= b 0)) 0)
+	(else (error "LOGICAL-AND" "Invalid signals" a b))))
