1c8dec03c2b56d0ce2ce093e069f7e4b13bb9241
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-05-26 15:33:46 +0800

    add a sample simulation

--- chapter3.scm
+++ chapter3.scm
@@ -1018,3 +1018,29 @@
   ((wire 'set-signal!) new-value))
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
+
+;; a sample simulation
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+		  action
+		  the-agenda))
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+	(first-item)
+	(remove-first-agenda-item! the-agenda)
+	(propagate))))
+(define (probe name wire)
+  (add-action! wire
+	       (lambda ()
+		 (newline)
+		 (display name)
+		 (display " ")
+		 (display (current-time the-agenda))
+		 (display " New-value = ")
+		 (display (get-signal wire)))))
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
