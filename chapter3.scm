bd923c7ca52bec215dc067bdfad1823f28cd304c
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-06-05 10:04:07 +0800

    add enumerate-interval and filter for stream

--- chapter3.scm
+++ chapter3.scm
@@ -1462,3 +1462,15 @@
   (car stream))
 (define (stream-cdr s)
   (my-force (cdr stream)))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low
+		   (stream-enumerate-interval (+ low 1) high))))
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+	((pred (stream-car stream))
+	 (cons-stream (stream-car stream)
+		      (stream-filter pred (stream-cdr stream))))
+	(else (stream-filter pred (stream-car stream)))))
