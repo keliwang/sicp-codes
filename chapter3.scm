5d48aff0357630ee5baa432e86e1a7717f283a64
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-06-05 09:57:55 +0800

    add stream's implementation

--- chapter3.scm
+++ chapter3.scm
@@ -1428,3 +1428,37 @@
 		 (the-mutex 'acquire)))
 	    ((eq? m 'release)
 	     (clear! cell))))))
+
+;; Streams
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+		   (stream-map proc (stream-cdr s)))))
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+	     (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+(define (display-line x)
+  (newline)
+  (display x))
+
+;; Must be a special form,
+;; or a and b will be evaluated
+;; when applying cons-stream.
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((cons-stream a b)
+     (cons a (my-delay b)))))
+(define (stream-car s)
+  (car stream))
+(define (stream-cdr s)
+  (my-force (cdr stream)))
