4ec432a3ae4983905f1dcf30831c748ba0edac8f
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-06-09 10:00:16 +0800

    exercise 3.60 done

--- chapter3.scm
+++ chapter3.scm
@@ -1718,13 +1718,21 @@
 
 ;; Exercise 3.59
 (define (integrate-series stream)
-  (stream-map-multi-streams / s integers))
+  (stream-map-multi-streams / stream integers))
 
 (define exp-series
   (cons-stream 1 (integrate-series exp-series)))
-(define consine-series
+(define cosine-series
   (cons-stream 1 (neg-stream (integrate-series sine-series))))
 (define sine-series
-  (cons-stream 0 (integrate-series consine-series)))
+  (cons-stream 0 (integrate-series cosine-series)))
 (define (neg-stream s)
   (stream-map (lambda (x) (- x)) s))
+
+;; Exercise 3.60
+(define (mul-series s1 s2)
+  (cons-stream
+   (* (stream-car s1) (stream-car s2))
+   (add-streams
+    (scale-stream (stream-cdr s2) (stream-car s1))
+    (mul-series (stream-cdr s1) s2))))
