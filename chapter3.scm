5e2e37c5619610ac748303b66b07ae1d1d49a192
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-06-11 10:54:47 +0800

    add some transform functions

--- chapter3.scm
+++ chapter3.scm
@@ -1775,3 +1775,16 @@
 	       (stream-map - (pi-summands (+ n 2)))))
 (define pi-stream
   (scale-stream (partial-sum (pi-summands 1)) 4))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+	(s1 (stream-ref s 1))
+	(s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+			  (+ s0 (* -2 s1) s2)))
+		 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s (make-tableau transform (transform s))))
+(define (accelerated-sequence transform s)
+  (stream-map stream-car (make-tableau transform s)))
