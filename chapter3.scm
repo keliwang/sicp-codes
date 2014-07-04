d5d5c05211982ca1c3d6afdf2851ff5142e04ca0
Edgar Wang <edgar.xiaoxiangyu@gmail.com>
2014-07-04 16:03:01 +0800

    exercise 3.75 done

--- chapter3.scm
+++ chapter3.scm
@@ -2001,3 +2001,24 @@
   (stream-map-multi-streams sign-change-detector
 			    sense-data
 			    (cons-stream 0 sense-data)))
+
+;; Exercise 3.75
+;; (define (make-zero-crossings input-stream last-value)
+;;   (let ((avpt (/ (+ stream-car input-stream)
+;; 		 last-value)
+;; 	      2)))
+;;   (cons-stream
+;;    (sign-change-detector avpt last-value)
+;;    (make-zero-crossings
+;;     (stream-cdr input-stream) avpt)))
+
+;; (define (make-zero-crossings input-stream last-value last-avpt)
+;;   (let ((avpt (/ (+ stream-car input-stream)
+;; 		 last-value)
+;; 	      2)))
+;;   (cons-stream
+;;    (sign-change-detector avpt last-avpt)
+;;    (make-zero-crossings
+;;     (stream-cdr input-stream)
+;;     (stream-car input-stream)
+;;     avpt)))
